40a13208b08c840662f806bf963b4b938eb1607d
임하나 <hanadul1@douzone.com>
2021-07-20 15:59:09 +0900

    문자열에서 문자 추출하기 (검색창에 입력하는 글자와 일치하는 검색결과 나타내기)

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const targetString = 'JavaScript를 배우자';
+// 검색어
+const searchWordText = document.querySelector('#search-word-input');
 
-document.querySelector('.result1').innerHTML
-=targetString.includes('JavaScript');
-document.querySelector('.result2').innerHTML
-=targetString.includes('배우자');
-document.querySelector('.result3').innerHTML
-=targetString.includes('배우자', 12);
-document.querySelector('.result4').innerHTML
-=targetString.includes('HTML');
-document.querySelector('.result5').innerHTML
-=targetString.startsWith('배우자');
-document.querySelector('.result6').innerHTML
-=targetString.endsWith('배우자');
+// 지역리스트
+const prefectureList = document.querySelectorAll('#prefecture-list button');
+
+// 문자가 입력될 때마다 데이터 체크 작업 실행
+searchWordText.addEventListener('keyup', () => {
+    // 입력한 검색어
+    const searchWord = searchWordText.value;
+
+    //지역 리스트의 루프 처리
+    //element는 각 요소에 해당한다
+    prefectureList.forEach((element) => {
+        // 검색어가 없으면 모든 요소를 표시
+        if (!searchWord || searchWord === '') {
+            element.classList.remove('hide');
+            return;
+        }
+
+        //데이터 name 가져오기
+        const prefectureName = element.dataset.name;
+        //데이터 영문 name 가져오기
+        const phonetic = element.dataset.phonetic;
+
+        // 검색어와 첫번째 글자 일치 여부에 따라 hide 클래스 사용 결정
+        // hide 클래스가 사용된 요소는 화면에 표시하지 않는다.
+        if (
+            searchWord.charAt(0) === prefectureName.charAt(0) ||
+            searchWord.charAt(0) === phonetic.charAt(0)
+        ) {
+            // 검색어의 첫 번째 글자가 일치하는 경우 hide 클래스 제거
+            element.classList.remove('hide');
+        } else {
+            // 검색어의 첫 번째 글자가 일치하지 않는 경우 hide 클래스를 추가
+            element.classList.add('hide');
+        }
+    })
+})
